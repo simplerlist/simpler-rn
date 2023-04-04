c139737ba5d95723dec835ea89e0061fe281013d
Martin Adams <martin@martinadams.com>
2023-04-04 10:08:45 -0700

    Init 🚀

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
+// Packages
+import { useCallback } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync()
 
 const App = () => {
+	const [fontsLoaded] = useFonts({
+		Inter: require('./assets/fonts/Inter-Regular.ttf')
+	})
+
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded) await SplashScreen.hideAsync()
+	}, [fontsLoaded])
+
+	if (!fontsLoaded) return null
+
 	return (
-		<View className='flex-1 justify-center items-center bg-slate-500'>
-			<Text>Open up App.js to start working on your app!</Text>
+		<View className='font-inter flex-1 justify-center items-center' onLayout={onLayoutRootView}>
+			<Text className='text-xl font-bold'>Open up App.js to start working on your app!</Text>
 
 			<StatusBar style='auto' />
 		</View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
 	content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {},
+		fontFamily: {
+			inter: ['Inter']
+		}
 	},
 	plugins: []
 }
